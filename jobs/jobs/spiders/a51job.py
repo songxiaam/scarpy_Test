ae7d480294d9e508a8cd32be5837e3fdbdd89078
Mike Zhao <954128120@qq.com>
2019-03-13 19:51:30 +0800

    优化

--- jobs/jobs/spiders/a51job.py
+++ jobs/jobs/spiders/a51job.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 import scrapy
-
+import re
 
 class A51jobSpider(scrapy.Spider):
     name = '51job'
@@ -17,18 +17,6 @@ class A51jobSpider(scrapy.Spider):
 
 
     def parse(self, response):
-        # list_kw = ['数据分析师']F%
-        # for kw in list_kw:
-        #     url = 'https://search.51job.com/list/020000,000000,0000,00,9,99,%s,2,1.html?lang=c&stype=1&postchannel=0000&workyear=99&cotype=99&degreefrom=99&jobterm=99&companysize=99&lonlat=0%2C0&radius=-1&ord_field=0&confirmdate=9&fromType=&dibiaoid=0&address=&line=&specialarea=00&from=&welfare='%
-        # (kw)
-        #     print("===========")
-        #     print(url)
-        #     yield scrapy.Request(url=url, callback=self.parse_list, dont_filter=True)
-
-
-        # url = "https://search.51job.com/list/020000,000000,0000,00,9,99,数据分析师,2,1.html?lang=c&stype=1&postchannel=0000&workyear=99&cotype=99&degreefrom=99&jobterm=99&companysize=99&lonlat=0%2C0&radius=-1&ord_field=0&confirmdate=9&fromType=&dibiaoid=0&address=&line=&specialarea=00&from=&welfare="
-        # yield scrapy.Request(url=url, callback=self.parse_list, dont_filter=True)
-
         selector = scrapy.Selector(response)
         result_list = selector.xpath('//*[@id="resultList"]/div[@class="el"]')
         for div_item in result_list:
@@ -56,12 +44,44 @@ class A51jobSpider(scrapy.Spider):
         item_cn = selector.xpath('/html/body/div[@class="tCompanyPage"]/div[@class="tCompany_center clearfix"]/div[@class="tHeader tHjob"]/div/div[@class="cn"]')
         # 1.职位名称
         # job_name = selector.xpath('/html/body/div[@class="tCompanyPage"]/div[@class="tCompany_center clearfix"]/div[@class="tHeader tHjob"]/div/div[@class="cn"]/h1/@title').extract()
-        job_name = item_cn.xpath('h1/@title').extract()
+        job_name = item_cn.xpath('h1/@title').extract()[0]
         # 2.公司名称
-        company_name = item_cn.xpath('p[@class="cname"]/a[1]/@title').extract()
+        company_name = item_cn.xpath('p[@class="cname"]/a[1]/@title').extract()[0]
         # 3.福利
         welfares = item_cn.xpath('div/div/span/text()').extract()
         # 4.月薪
-        monthly_pay = item_cn.xpath('strong/text()').extract()
-        print(job_name[0]+"--"+company_name[0]+','.join(welfares)+'--'+monthly_pay[0])
+        monthly_pay = item_cn.xpath('strong/text()').extract()[0]
+        # 5.职位要求
+        requirement = item_cn.xpath('p[2]/@title').extract()[0].replace(u'\xa0', u' ').replace(' ', '').split('|') # &nbsp解码
 
+
+        item_detail = selector.xpath('/html/body/div[@class="tCompanyPage"]/div[@class="tCompany_center clearfix"]/div[3]')
+        # 6.职位详情
+        job_details = item_detail.xpath('div[1]/div[1]/p/text()').extract()
+        # 7.上班地址
+        job_address = item_detail.xpath('div[2]/div/p/text()').extract()
+        # 8.地图位置
+        job_positions = self.sub_string(item_detail.xpath('div[2]/div/a/@onclick').extract()[0])
+        job_map = ''
+        if job_positions:
+            job_map = job_positions[0]
+
+        item_company = selector.xpath('/html/body/div[3]/div[2]/div[4]/div[1]')
+        # 9.公司名称
+        company_name = item_company.xpath('div[1]/a/p/@title').extract()[0]
+        # 10.公司链接
+        company_link = ''
+        company_links = item_company.xpath('div[1]/a/@href').extract()
+        if company_links:
+            company_link = company_links[0]
+        # 11.公司性质
+        company_nature = item_company.xpath('div[2]/p[1]/text()').extract()[0]
+        # 12.员工人数
+        company_people = item_company.xpath('div[2]/p[2]/text()').extract()[0]
+        # 13.所处行业
+        company_industry = item_company.xpath('div[2]/p[3]/a/text()').extract()
+        print(job_name+"--"+company_name+','.join(welfares)+'--'+monthly_pay+'--'+','.join(requirement)+'\n'+job_address[1]+job_map+'\n'+' '.join(company_industry)+company_link)
+
+    def sub_string(self, template):
+        rule = r"'(.*?)'"
+        return re.findall(rule, template)
